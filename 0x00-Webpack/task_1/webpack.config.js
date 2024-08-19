441dd97e246ba09e19feea70e430d8ce3cd47d2e
Jocseh07 <josephmkaranja07@gmail.com>
2024-08-19 19:37:38 +0300

    add task

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,12 +1,10 @@
-const path = require('path');
+import { resolve } from 'path';
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
+export const mode = 'production';
+export const entry = {
+  main: resolve(__dirname, './js/dashboard_main.js'),
+};
+export const output = {
+  filename: 'bundle.js',
+  path: resolve(__dirname, 'public'),
 };
