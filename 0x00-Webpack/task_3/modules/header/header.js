551c74ecf075b22cbf297fe53b216ff3a78b76f2
Jocseh07 <josephmkaranja07@gmail.com>
2024-08-19 21:52:17 +0300

    add task

--- 0x00-Webpack/task_3/modules/header/header.js
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -2,7 +2,6 @@ import $ from 'jquery';
 import './header.css';
 
 $('body').append('<header></header>');
-$('header').append('<div id="logo"></div>');
-$('header').append('<h1>Holberton Dashboard</h1>');
+$('header').append('<div id="logo"></div>', '<h1>Holberton Dashboard</h1>');
 
 console.log('Init header');
