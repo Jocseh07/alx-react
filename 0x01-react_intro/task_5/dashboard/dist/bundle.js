564a526078811fa35bce18b5158cae723741e5ba
Jocseh07 <josephmkaranja07@gmail.com>
2024-08-21 20:07:31 +0300

    build

---
+++ 0x01-react_intro/task_5/dashboard/dist/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdGFza181Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoJ2JvZHknKS5hcHBlbmQoJzxwPkhvbGJlcnRvbiBEYXNoYm9hcmQ8L3A+Jyk7XG4kKCdib2R5JykuYXBwZW5kKCc8cD5EYXNoYm9hcmQgZGF0YSBmb3IgdGhlIHN0dWRlbnRzPC9wPicpO1xuJCgnYm9keScpLmFwcGVuZCgnPGJ1dHRvbj5DbGljayBoZXJlIHRvIGdldCBzdGFydGVkPC9idXR0b24+Jyk7XG4kKCdib2R5JykuYXBwZW5kKCc8cCBpZD1cImNvdW50XCI+PC9wPicpO1xuJCgnYm9keScpLmFwcGVuZCgnPHA+Q29weXJpZ2h0IC0gSG9sYmVydG9uIFNjaG9vbDwvcD4nKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
